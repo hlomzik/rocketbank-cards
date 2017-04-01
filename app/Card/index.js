2b6bc6ccd5c94a115b2717f449323ed672086685
hlomzik <hlomzik@gmail.com>
2017-04-02 02:49:54 +0300

    Improve Card's code, add spaces and shadow
    Add prop types via flow type.
    Add spaces between groups of 3 digits and before currency.
    Add shadow for cards (and reorder styles).

--- app/Card/index.js
+++ app/Card/index.js
@@ -7,26 +7,35 @@ import {
   View
 } from 'react-native'
 
-const Card = ({ name, amount, currency }) => (
+type CardProps = { name: string, amount: string|number, currency: string }
+
+const Card = ({ name, amount, currency }: CardProps) => (
   <View style={styles.card}>
     <Text style={styles.name}>{name}</Text>
     <Text style={styles.money}>
-      <Text style={styles.amount}>{amount}</Text>
-      <Text style={styles.currency}>{currency}</Text>
+      <Text style={styles.amount}>{formatAmount(amount)}</Text>
+      <Text style={styles.currency}> {currency}</Text>
     </Text>
   </View>
 )
 
 export default Card
 
+const formatAmount = amount => ('' + amount)
+  // put space after digits followed by groups of 3 digits only
+  .replace(/(\d+)(?=(\d{3})+(\D|$))/, '$1 ')
+
 const styles = StyleSheet.create({
   card: {
-    justifyContent: 'space-between',
     backgroundColor: 'orange',
+    borderRadius: 10,
     height: 200,
+    justifyContent: 'space-between',
     margin: 20,
     padding: 25,
-    borderRadius: 10
+    shadowColor: 'black',
+    shadowRadius: 5,
+    shadowOpacity: 0.3
   },
   name: {
     color: 'white',
