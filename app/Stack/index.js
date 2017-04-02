3184527576b4a6de2a13873496debe86db59d87b
hlomzik <hlomzik@gmail.com>
2017-04-02 03:44:28 +0300

    Add some 3d effect - just move cards
    Just move down cards on scroll - first cards move faster.
    There are bugs because of missed work with content height.

--- app/Stack/index.js
+++ app/Stack/index.js
@@ -6,11 +6,39 @@ import { ScrollView } from 'react-native'
 import ScrollItem from './Item'
 
 type StackProps = { children?: Element[] }
+// Event don't have contentOffset :(
+type ScollEvent = { nativeEvent: Event & { contentOffset: { y: number }} }
 
-const Stack = ({ children }: StackProps) => (
-  <ScrollView>{children && children.map((child, i) => (
-    <ScrollItem key={i} gap={20 + i * 50}>{child}</ScrollItem>
-  ))}</ScrollView>
-)
+export default class Stack extends React.Component {
+  props: StackProps
 
-export default Stack
+  state = { shift: 0 }
+
+  onScroll = (e: ScollEvent) => {
+    const y = e.nativeEvent.contentOffset.y
+    console.log(y, e.nativeEvent)
+    // there is a problem with changing contentSize.height
+    // and contentOffset.y sometimes dropped to zero,
+    // so for now just ignore such cases and think about height;
+    // also ignore scrolling up (y > 0)
+    if (y < 0) this.setState({ shift: -y })
+  }
+
+  render() {
+    const items = this.props.children || []
+
+    return (
+      // call it every frame (once in 16ms)
+      <ScrollView onScroll={this.onScroll} scrollEventThrottle={16}>{
+        items.map((child, i) => (
+          <ScrollItem
+            key={i}
+            gap={this.state.shift * (i + 1)}
+            >
+            {child}
+          </ScrollItem>
+        ))
+      }</ScrollView>
+    )
+  }
+}
